f2836345ff8c33ace4706f7803a0b87a80f75eb6
drorbn <drorbn@af7a50df-ec00-0410-9275-b2b6c1c1078d>
2005-11-06 02:56:23 +0000

    vi

--- KnotTheory/init.m
+++ KnotTheory/init.m
@@ -24,7 +24,7 @@ location on the host computer. It can be reset by the user.
 
 Begin["`System`"]
 
-KnotTheoryVersion[] = {2005,11,05,21,28,39.137210000};
+KnotTheoryVersion[] = {2005,11,05,21,53,40.253480000};
 KnotTheoryVersion[k_Integer] := KnotTheoryVersion[][[k]]
 
 KnotTheoryVersionString[] = StringJoin[
@@ -57,7 +57,6 @@ KnotTheoryWelcomeMessage[] = StringJoin[
 Print[KnotTheoryWelcomeMessage[]]
 
 End[]; EndPackage[];
-
 
 BeginPackage["KnotTheory`"]
 
