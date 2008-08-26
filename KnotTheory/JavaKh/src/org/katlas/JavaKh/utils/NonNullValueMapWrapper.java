748c693dba9d6b41b29931e874209b972fdb2b09
Scott Morrison <scott@tqft.net>
2008-08-26 20:30:05 +0000

    remove dumb @override statements

--- KnotTheory/JavaKh/src/org/katlas/JavaKh/utils/NonNullValueMapWrapper.java
+++ KnotTheory/JavaKh/src/org/katlas/JavaKh/utils/NonNullValueMapWrapper.java
@@ -17,42 +17,34 @@ public class NonNullValueMapWrapper<K, V> implements Map<K, V> {
 		this.internalMap = internalMap;
 	}
 	
-	@Override
 	public void clear() {
 		internalMap.clear();
 	}
 
-	@Override
 	public boolean containsKey(Object key) {
 		return internalMap.containsKey(key);
 	}
 
-	@Override
 	public boolean containsValue(Object value) {
 		return internalMap.containsValue(value);
 	}
 
-	@Override
 	public Set<java.util.Map.Entry<K, V>> entrySet() {
 		return internalMap.entrySet();
 	}
 
-	@Override
 	public V get(Object key) {
 		return internalMap.get(key);
 	}
 
-	@Override
 	public boolean isEmpty() {
 		return internalMap.isEmpty();
 	}
 
-	@Override
 	public Set<K> keySet() {
 		return internalMap.keySet();
 	}
 
-	@Override
 	public V put(K key, V value) {
 		if(value == null) {
 			throw new IllegalArgumentException("Tried to add a null value.");
@@ -60,24 +52,20 @@ public class NonNullValueMapWrapper<K, V> implements Map<K, V> {
 		return internalMap.put(key, value);
 	}
 
-	@Override
 	public void putAll(Map<? extends K, ? extends V> m) {
 		for(K k : m.keySet()) {
 			put(k, m.get(k));
 		}
 	}
 
-	@Override
 	public V remove(Object key) {
 		return internalMap.remove(key);
 	}
 
-	@Override
 	public int size() {
 		return internalMap.size();
 	}
 
-	@Override
 	public Collection<V> values() {
 		return internalMap.values();
 	}
