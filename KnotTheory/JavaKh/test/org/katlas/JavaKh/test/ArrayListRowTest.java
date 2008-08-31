70baca697614532496f9c22e71a9537441aaa402
Scott Morrison <scott@tqft.net>
2008-08-31 04:49:19 +0000

    yet another implementation of MatrixRow

---
+++ KnotTheory/JavaKh/test/org/katlas/JavaKh/test/ArrayListRowTest.java
@@ -0,0 +1,14 @@
+package org.katlas.JavaKh.test;
+
+
+import org.junit.Before;
+import org.katlas.JavaKh.rows.ArrayListRow;
+
+public class ArrayListRowTest extends MatrixRowTest<ArrayListRow<String>> {
+
+	@Before
+	public void setUp() throws Exception {
+		row = new ArrayListRow<String>();
+	}
+
+}
